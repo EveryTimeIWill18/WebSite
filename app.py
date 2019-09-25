1f0b569de13be65886b85c822b2302154aad7ccc
William Murphy <30298478+everytimeiwill18@users.noreply.github.com>
2019-09-25 09:05:56 -0400

    Added login.html, base.html, about.html

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {'author': ' William Robert Murphy',
+     'title': 'Blog Post 1',
+     'content': 'First Blog Post',
+     'date_posted': 'September 25, 2019'}
+]
+
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('login.html')
 
+@app.route('/about')
+def about():
+    return render_template("about.html", posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
